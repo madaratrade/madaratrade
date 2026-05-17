3586aa6ba26b7781b8258cc774f3a25ef0c45363
aggram <ammighorbani13851@gmail.com>
2026-05-17 23:37:50 +0000

    Update / fix

--- src/chat.php
+++ src/chat.php
@@ -1,6 +1,11 @@
 <?php
 session_start();
 
+if(!isset($_SESSION['user_id'])) {
+	    header("Location: login.php");
+	        exit;
+}
+
 $sender_id = $_SESSION['user_id'];
 $username  = $_SESSION['username'];
 $uuid      = $_SESSION['uuid'];
